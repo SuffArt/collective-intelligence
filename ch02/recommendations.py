3d263802c13e73bdaf46c1bff78a150fb43ca6af
Alexey A. Villas Bôas <aboas@thoughtworks.com>
2013-09-23 14:13:25 -0300

    MovieLens dataset loading function

--- ch02/recommendations.py
+++ ch02/recommendations.py
@@ -178,4 +178,19 @@ def getRecommendedItems(prefs,itemMatch,user):
   return rankings
 
 
+def loadMovieLens(path='movie_lens'):
+  # Get movie titles
+  movies={}
+  for line in open(path+'/u.item'):
+    (id,title)=line.split('|')[0:2]
+    movies[id]=title
+  
+  # Load data
+  prefs={}
+  for line in open(path+'/u.data'):
+    (user,movieid,rating,ts)=line.split('\t')
+    prefs.setdefault(user,{})
+    prefs[user][movies[movieid]]=float(rating)
+  return prefs
+
 
