cfc0b4441e13e7b5ac8ef66c9b93b74a355d2c62
Alexey A. Villas Bôas <aboas@thoughtworks.com>
2013-09-30 15:29:16 -0300

    kmeans

--- ch03/clusters.py
+++ ch03/clusters.py
@@ -1,5 +1,6 @@
 from math import sqrt
 from PIL import Image,ImageDraw
+import random
 
 def readfile(filename):
   lines=[line for line in file(filename)]
@@ -145,12 +146,70 @@ def rotatematrix(data):
   return newdata
 
 
-rownames, colnames, data = readfile ('blogdata.txt')
-cluster = hcluster(data)
-printclust (cluster, rownames)
-drawdendrogram(cluster, rownames)
+def buildClustersBlogs(rownames, colnames, data):
+  cluster = hcluster(data)
+  printclust (cluster, rownames)
+  drawdendrogram(cluster, rownames)
 
-rdata = rotatematrix(data)
-wordclusters = hcluster(rdata)
-drawdendrogram (wordclusters, colnames, 'wordclusters.png')
+def buildClustersWords(rownames, colnames, data):
+  rdata = rotatematrix(data)
+  wordclusters = hcluster(rdata)
+  drawdendrogram (wordclusters, colnames, 'wordclusters.png')
+
+
+# ***************************************
+# ************ K-Means ******************
+# ***************************************
+
+def kcluster(rows,distance=pearson,k=4):
+  # Determine the minimum and maximum values for each point
+  ranges=[(min([row[i] for row in rows]),max([row[i] for row in rows])) for i in range(len(rows[0]))]
+  # Create k randomly placed centroids
+  clusters=[[random.random( )*(ranges[i][1]-ranges[i][0])+ranges[i][0] for i in range(len(rows[0]))] for j in range(k)]
+  
+  lastmatches=None
+  for t in range(100):
+    print 'Iteration %d' % t
+    bestmatches=[[] for i in range(k)]
+    # Find which centroid is the closest for each row
+    for j in range(len(rows)):
+      row=rows[j]
+      bestmatch=0
+      for i in range(k):
+        d=distance(clusters[i],row)
+        if d<distance(clusters[bestmatch],row): bestmatch=i
+      bestmatches[bestmatch].append(j)
+    
+    # If the results are the same as last time, this is complete
+    if bestmatches==lastmatches: break
+    lastmatches=bestmatches
+  
+    # Move the centroids to the average of their members
+    for i in range(k):
+      avgs=[0.0]*len(rows[0])
+      if len(bestmatches[i])>0:
+        for rowid in bestmatches[i]:
+          for m in range(len(rows[rowid])):
+            avgs[m]+=rows[rowid][m]
+        for j in range(len(avgs)):
+          avgs[j]/=len(bestmatches[i])
+        clusters[i]=avgs
+  
+  return bestmatches
+
+def buildKCluster(rownames, colnames, data):
+	kclust = kcluster(data)
+
+	for centroidIndex in range(len(kclust)):
+		print ("Centroid %i" % centroidIndex)
+		print [rownames[blog] for blog in kclust[centroidIndex]]
+
+
+
+# ***** Main ********
+rownames, colnames, data = readfile ('blogdata.txt')
+
+# buildClustersBlogs()
+# buildClustersWords()
+buildKCluster(rownames, colnames, data)
 
