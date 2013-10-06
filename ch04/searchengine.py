20a6567ecac61f16c2dd83b9a2a5412a5dd1fb42
Alexey A. Villas Bôas <aboas@thoughtworks.com>
2013-10-06 14:17:32 -0300

    Adding search engine skeleton

---
+++ ch04/searchengine.py
@@ -0,0 +1,26 @@
+class crawler:
+# Initialize the crawler with the name of database 
+def __init__(self,dbname):
+  pass
+
+def __del__(self): 
+  pass
+
+def dbcommit(self):
+  pass
+  
+# Auxilliary function for getting an entry id and adding
+ # it if it's not present
+def getentryid(self,table,field,value,createnew=True):
+  return None
+  
+# Index an individual page
+def addtoindex(self,url,soup):
+  print 'Indexing %s' % url
+
+# Extract the text from an HTML page (no tags)
+def gettextonly(self,soup):
+  return None
+  # Separate the words by any non-whitespace character
+def separatewords(self,text):
+  return None
